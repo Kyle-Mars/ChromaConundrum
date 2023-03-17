f621451443dbf477d187a2dcdbe10ea7c7191750
ThePerfectPixl <kylelmars@gmail.com>
2023-03-16 21:18:34 -0500

    Added tutorial stage

--- ChromaConundrum/Assets/Scripts/SceneChange.cs
+++ ChromaConundrum/Assets/Scripts/SceneChange.cs
@@ -5,12 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
-    void Start()
-    {
-        Scene scene = SceneManager.GetActiveScene();
-        if (scene.name != "Start") ChangeScene("Start");
-    }
-
     static public void ChangeScene(string scenename)
     {
         SceneManager.LoadScene(scenename);
